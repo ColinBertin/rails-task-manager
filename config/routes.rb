9ba2174b6ad0d582b653130f758151cc112b4171
Colin Bertin <colinbertin12@gmail.com>
2022-02-15 16:43:10 +0900

    refacto form (but not finish)

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
-  # Display all the tasks
-  get '/tasks', to: 'tasks#index'
+  # # Display all the tasks
+  # get '/tasks', to: 'tasks#index'
 
-  # Create a new task
-  get '/tasks/new', to: 'tasks#new', as: :new_task
-  post 'tasks', to: 'tasks#create'
+  # # Create a new task
+  # get '/tasks/new', to: 'tasks#new', as: :new_task
+  # post 'tasks', to: 'tasks#create'
 
-  # Edit the task
-  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch 'tasks/:id', to: 'tasks#update'
+  # # Edit the task
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
 
-  # Delete task
-  delete '/tasks/:id', to: 'tasks#destroy'
+  # # Delete task
+  # delete '/tasks/:id', to: 'tasks#destroy'
 
-  # Display the task selected only
-  get '/tasks/:id', to: 'tasks#show', as: :task
-
+  # # Display the task selected only
+  # get '/tasks/:id', to: 'tasks#show', as: :task
+  resources :tasks
 end
